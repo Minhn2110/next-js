c4ac7b3e5bd7be48aee0c064c59952759ae57fcc
DESKTOP-CV7BJH8\Rex <minhnha@smartosc.com>
2021-12-17 11:35:04 +0700

    a

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Minh</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>dasdasdasdsadasd</div>
+    </>
+  );
+};
+
+export default About;
